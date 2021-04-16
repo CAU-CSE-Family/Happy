2fe8eccdd3da0059b6fad41f16982611d047993b
Sudal <56054637+Sudal1@users.noreply.github.com>
2021-04-17 00:11:50 +0900

    fix some issue

--- server/public/rand.js
+++ server/public/rand.js
@@ -1,25 +1,17 @@
-var randomNum = {};
-
-randomNum.random = function (n1, n2) {
-    return parseInt(Math.random() * (n2 - n1 + 1)) + n1
+exports.authNo = function (n) {
+  var value = ""
+  for (var i = 0; i < n; i++) {
+    value += parseInt(Math.random() * (10))
+  }
+  return value
 }
 
-randomNum.authNo = function (n) {
-    var value = ""
-    for (var i = 0; i < n; i++) {
-        value += randomNum.random(0, 9)
-    }
-    return value
-};
-
-exports.randomString = async function (length){
-    var result           = [];
-    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
-    var charactersLength = characters.length
-    for ( var i = 0; i < length; i++ ) {
-      result.push(characters.charAt(Math.floor(Math.random() * charactersLength)))
-   }
-   return result.join('')
+exports.randomString = function (length){
+  var result           = [];
+  var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  var charactersLength = characters.length
+  for ( var i = 0; i < length; i++ ) {
+    result.push(characters.charAt(Math.floor(Math.random() * charactersLength)))
   }
-
-module.exports = randomNum
+  return result.join('')
+}
