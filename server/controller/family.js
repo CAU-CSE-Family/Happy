311f9080f16bbef5a719fc7938944d31576d85ef
Sudal <56054637+Sudal1@users.noreply.github.com>
2021-04-17 19:59:20 +0900

    fix create family function correctly

--- server/controller/family.js
+++ server/controller/family.js
@@ -1,34 +1,29 @@
-const rand = require("../public/rand.js")
-const User = require('../models/user')
+const rand     = require("../public/rand.js")
+const User     = require('../models/user')
 
 exports.createFamily = async function (req, res){
   console.log("Create Family:\n", req.body)
 
   const googleId = req.body["id"]
   const sessionKey = req.body["session"]
-    
-  User.findOne({ id: googleId, seesion: sessionKey }).then(existingUser => {
+  
+  let familyId = ""
+  do {
+    familyId = rand.randomString(16)
+    if (User.find({ id_family: familyId }).count() > 0) {
+      familyId = ""
+    } else { break }
+  } while (true)
+  console.log("Successfully created family ID: ", familyId)
+
+  User.findOneAndUpdate({ id: googleId, session: sessionKey }, {
+      $set : { id_family: familyId }
+    }).then(existingUser => {
     if (!existingUser) {
       console.log("No matching user ID and token in the DB.")
       res.json({result: false, message: "ID와 Token이 유효하지 않습니다."})
     }
     else if (existingUser) {
-      let familyId = ""
-      do {
-        familyId = rand.randomString(16)
-        User.findOne({ id_family: familyId }).then(() => {
-          familyId = null
-        })
-      } while (familyId)
-      console.log("Successfully created family ID: ", familyId)
-    
-      User.findByIdAndUpdate({ id: googleId }, {
-        $set : { id_family: familyId }
-      }, (err) => {
-        console.log(err)
-        res.json({result: false, message: "Error: Can't update user DB"})
-      })
-
       const MemberData = {
         "id": existingUser.id,
         "profileData": ProfileData = {
@@ -52,13 +47,13 @@ exports.joinFamily = async function (req, res){
   const sessionKey = req.body["session"]
   const familyId = req.body["id_family"]
 
-  User.findOne({ id: googleId, seesion: sessionKey }).then(existingUser => {
+  User.findOne({ id: googleId, session: sessionKey }).then(existingUser => {
     if (!existingUser) {
       console.log("No matching user ID and token in the DB.")
       res.json({result: false, message: "ID와 Token이 유효하지 않습니다."})
     }
     else if (existingUser) {
-      User.find({ id_family: familyId }).toArray().then(existingFamily => {
+      User.find({ id_family: familyId }).then(existingFamily => {
         if (!existingFamily) {
           console.log("No matching family ID in the DB.")
           res.json({result: false, message: "매칭되는 family ID가 없습니다."})
