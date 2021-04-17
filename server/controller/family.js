7ae64c733d6d179c5ba0d0f977c340166e474742
Sudal <56054637+Sudal1@users.noreply.github.com>
2021-04-17 20:22:28 +0900

    fix join family function as same above commit

--- server/controller/family.js
+++ server/controller/family.js
@@ -16,9 +16,9 @@ exports.createFamily = async function (req, res){
   } while (true)
   console.log("Successfully created family ID: ", familyId)
 
-  User.findOneAndUpdate({ id: googleId, session: sessionKey }, {
-      $set : { id_family: familyId }
-    }).then(existingUser => {
+  User.findOneAndUpdate({ id: googleId, session: sessionKey },
+    { $set : { id_family: familyId } }
+  ).then(existingUser => {
     if (!existingUser) {
       console.log("No matching user ID and token in the DB.")
       res.json({result: false, message: "ID와 Token이 유효하지 않습니다."})
@@ -46,47 +46,50 @@ exports.joinFamily = async function (req, res){
   const googleId = req.body["id"]
   const sessionKey = req.body["session"]
   const familyId = req.body["id_family"]
+  const members = []
 
-  User.findOne({ id: googleId, session: sessionKey }).then(existingUser => {
+  User.find({ id_family: familyId }).then(existingFamily => {
+    if (!existingFamily) {
+      console.log("No matching family ID in the DB.")
+      res.json({result: false, message: "매칭되는 family ID가 없습니다."})
+    }
+    else if (existingFamily) {
+      existingFamily.forEach(member => {
+        const MemberData = {
+          "id": member.id,
+          "profileData": ProfileData = {
+            "name": member.name,
+            "phone": member.phone,
+            "photoUrl": member.photo_url
+          }
+        }
+        members.push(MemberData)
+      })
+    }
+  }).catch(err => {
+    console.log(err)
+    res.json({result: false, message: err})
+  })
+
+  User.findOneAndUpdate({ id: googleId, session: sessionKey },
+    { $set : { id_family: familyId } }
+  ).then(existingUser => {
     if (!existingUser) {
       console.log("No matching user ID and token in the DB.")
       res.json({result: false, message: "ID와 Token이 유효하지 않습니다."})
     }
     else if (existingUser) {
-      User.find({ id_family: familyId }).then(existingFamily => {
-        if (!existingFamily) {
-          console.log("No matching family ID in the DB.")
-          res.json({result: false, message: "매칭되는 family ID가 없습니다."})
+      const MemberData = {
+        "id": existingUser.id,
+        "profileData": ProfileData = {
+          "name": existingUser.name,
+          "phone": existingUser.phone,
+          "photoUrl": existingUser.photo_url
         }
-        else if (existingFamily) {
-          console.log(existingFamily)
-          console.log(existingFamily[0])
-
-          User.findByIdAndUpdate({ id: googleId }, {
-            $set : { id_family: familyId }
-          }, (err) => {
-            console.log(err)
-            res.json({result: false, message: "Error: Can't update user to DB"})
-          })
-
-          const members = []
-          existingFamily.forEach(member => {
-            const MemberData = {
-              "id": member.id,
-              "profileData": ProfileData = {
-                "name": member.name,
-                "phone": member.phone,
-                "photoUrl": member.photo_url
-              }
-            }
-            members.push(MemberData)
-          })
-          res.json({result: true, message: "Successfully join family", family: familyId, members: members})
-        }
-      }).catch(err => {
-        console.log(err)
-        res.json({result: false, message: err})
-      })
+      }
+      members.push(MemberData)
+      
+      res.json({result: true, message: "Successfully join family", family: familyId, members: members})
     }
   }).catch(err => {
     console.log(err)
