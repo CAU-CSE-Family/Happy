c232f876ae5476c3e395e716ec198c718d55766e
Sudal <56054637+Sudal1@users.noreply.github.com>
2021-05-20 01:17:50 +0900

    fix

--- server/controller/family.js
+++ server/controller/family.js
@@ -18,8 +18,9 @@ exports.createFamily = async function (req, res){
   console.log("Create Family:\n", req.headers)
 
   const token = req.headers['authorization'].split(" ")[1]
-  console.log("token: ", token)
-  
+  token = token.replace("\"", "")
+  console.log(token)
+
   try {
     const payload = jwt.verify(token, process.env.SECRET_KEY)
     console.log("\npayload: ", payload)
