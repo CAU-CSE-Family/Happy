86bc3aaf8afd1ad5e2247d87e395e989bebe222a
Ubuntu <ubuntu@ip-172-31-45-230.us-east-2.compute.internal>
2021-05-19 16:18:19 +0000

    fix

--- server/controller/family.js
+++ server/controller/family.js
@@ -18,7 +18,7 @@ exports.createFamily = async function (req, res){
   console.log("Create Family:\n", req.headers)
 
   const token = req.headers['authorization'].split(" ")[1]
-  console.log("token: ", token)
+  console.log(token)
   
   try {
     const payload = jwt.verify(token, process.env.SECRET_KEY)
