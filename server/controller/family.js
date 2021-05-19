34cb2df6bf2b54e9c3ed7cd2913fb61767bf425a
Sudal <56054637+Sudal1@users.noreply.github.com>
2021-05-20 01:27:44 +0900

    fix

--- server/controller/family.js
+++ server/controller/family.js
@@ -23,6 +23,7 @@ exports.createFamily = async function (req, res){
 
   try {
     const payload = jwt.verify(token, `${process.env.SECRET_KEY}`)
+    console.log(`${process.env.SECRET_KEY}`)
     console.log("\npayload: ", payload)
     const googleId = payload['id']
     const familyId = createFamilyId()
