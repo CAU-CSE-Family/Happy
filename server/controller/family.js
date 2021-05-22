3eff2005e45b3bf491ab2236af040edbe2e26f2b
Sudal <56054637+Sudal1@users.noreply.github.com>
2021-05-22 20:05:57 +0900

    re-factoring all of the family.js functions with jwt

--- server/controller/family.js
+++ server/controller/family.js
@@ -15,8 +15,7 @@ function createFamilyId(){
 }
 
 exports.createFamily = async function (req, res){
-  console.log("Create Family:\n", req.id)
-
+  console.log("Create Family:" + req.id)
   try {
     const googleId = req.id
     const familyId = createFamilyId()
@@ -24,120 +23,101 @@ exports.createFamily = async function (req, res){
       id: familyId,
       user_list: { id_user: googleId }
     }
-    const newFamily = await new Family(clientFamily).save()
-    if (!newFamily) {
+
+    const response = await new Family(clientFamily).save()
+    console.log(response)
+    if (response.ok != 1) {
       res.status(401).json({ msg: "Error in DB on creating family" })
-    } else {
-      const user = await User.findOneAndUpdate(
-        { id: googleId },
-        { $set : { id_family: familyId } }
-      )
-      if (!user) {
-        const deletedFamily = await Family.deleteOne({ id: clientFamily.id })
-        console.log("User ID and session not vaild, delete family: ", deletedFamily)
-        res.status(401).json({ msg: "User ID and session not vaild" })
-      } else {
-        res.status(200).json({ familyId: familyId })
-      }
-    }
+    } 
+    
+    const response2 = await User.findOneAndUpdate(
+      { id: googleId },
+      { $set : { id_family: familyId } }
+    )
+    if (!response2) {
+      const response3 = await Family.deleteOne({ id: clientFamily.id })
+      console.log("User ID and session not vaild, delete family: ", response3)
+      res.status(401).json({ msg: "User ID and session not vaild" })
+    } 
+    res.status(200).json({ familyId: familyId })
   } catch (err) {
+    console.log(err)
     res.status(401).json({ msg: err })
   }
 }
 
 exports.joinFamily = async function (req, res){
-  console.log("Join Family:\n", req.id)
-
-  const googleId = req.id
-  const sessionKey = req.body.authData["session"]
-  const familyId = req.body["family"]
-  var tempFamilyId = ""
-  var result = true
-  var msg = "Successfully join family"
-
+  console.log("Join Family:" + req.id)
   try {
-    const user = await User.findOneAndUpdate(
-      { id: googleId, session: sessionKey },
-      { $set : { id_family: familyId } }
-    )
+    const googleId = req.id
+    const user = await member.getMember(googleId)
     if (!user) {
-      result = false
-      msg = "User ID and session not vaild"
+      res.status(400).json({ message: "Invalid user ID" })
     }
-    else {
-      tempFamilyId = user["id_family"]
-      try {
-        const family = await Family.findOneAndUpdate(
-          { id: familyId },
-          { $push : { user_list: { id_user: googleId } } }
-        )
-        if (!family) {
-          result = false
-          msg = "Family ID is not in DB"
-        }
-      } catch (err) {
-        console.log(err)
-        result = false
-        msg = "Error occured in DB"
-      }
+    const sessionKey = user.session
+    const familyId = user.id_family
+    const reqFamilyId = req.id_family
+
+    const response = await User.findOneAndUpdate(
+      { id: googleId, session: sessionKey },
+      { $set : { id_family: reqFamilyId } }
+    )
+    if (response.ok != 1) {
+      res.status(400).json({ message: "Invalid user ID" })
     }
+    const response2 = await Family.findOneAndUpdate(
+      { id: familyId },
+      { $push : { user_list: { id_user: googleId } } }
+    )
+    if (response2.ok != 1) {
+      await User.findOneAndUpdate(
+        { id: googleId, session: sessionKey },
+        { $set : { id_family: familyId } }
+      )
+      res.status(400).json({ message: "Invalid family ID" })
+    }
+    res.status(200).json({ family: familyId })
   } catch (err) {
     console.log(err)
-    result = false
-    msg = "Error occured in DB"
+    res.status(400).json({ message: "Error occured in DB" })
   }
-
-  if (result === false) {
-    await User.findOneAndUpdate(
-      { id: googleId, session: sessionKey },
-      { $set : { id_family: tempFamilyId } }
-    )
-  }
-
-  res.json({result: result, message: msg, family: familyId})
 }
 
 exports.leaveFamily = async function (req, res){
-  console.log("Leave Family:\n", req.body)
-
-  const googleId = req.body["id"]
-  const sessionKey = req.body["session"]
-  var result = true
-  var msg = "Successfully leave family"
-
+  console.log("Leave Family:" + req.id)
   try {
-    const user = await User.findOne({ id: googleId, session: sessionKey })
+    const googleId = req.id
+    const user = await member.getMember(googleId)
     if (!user) {
-      result = false
-      msg = "User ID and session not vaild"
-    } else {
-      try {
-        const family = await Family.findOneAndUpdate(
-          { id: user["id_family"] },
-          { $pull: { user_list: { id_user: googleId } } },
-          { upsert: false, multi: true }
-        )
-        console.log("Leave family ID: ", user["id_family"])
-        if (!family) {
-          result = false
-          msg = "Family ID not vaild"
-        } else {
-          await User.findOneAndUpdate(
-            { id: googleId, session: sessionKey },
-            { $set : { id_family: null } }
-          )
-        }
-      } catch (err) {
-        console.log(err)
-        result = false
-        msg = "Error occured in DB"
-      }
+      res.status(400).json({ message: "Invalid user ID" })
     }
+    const sessionKey = user.session
+
+    const response = await User.findOne({ id: googleId, session: sessionKey })
+    if (response.ok != 1) {
+      res.status(400).json({ message: "Invalid user ID" })
+    } 
+    
+    const response2 = await Family.findOneAndUpdate(
+      { id: user.id_family },
+      { $pull: { user_list: { id_user: googleId } } },
+      { upsert: false, multi: true }
+    )
+    console.log("Leave family ID: " + user.id_family)
+    if (response2.ok != 1) {
+      res.status(400).json({ message: "Invalid family ID" })
+    }
+
+    const response3 = await User.findOneAndUpdate(
+      { id: googleId, session: sessionKey },
+      { $set : { id_family: null } }
+    )
+    if (response3.ok != 1) {
+      res.status(400).json({ message: "Invalid user ID" })
+    }
+    res.status(200).json({ message: "Successfully leave family" })
   } catch (err) {
     console.log(err)
-    result = false
-    msg = "Error occured in DB"
+    res.status(400).json({ message: "Error occured in DB" })
   }
-  
-  res.json({result: result, message: msg})
 }
